6c91de83d49356ff13246aa3a333c07de5f6d912
Ejaz Shaikh <Ejaz.Shaikh@blueconchtech.com>
2021-05-23 19:34:08 +0530

    polling sensor added

--- sensors/sample.py
+++ sensors/sample.py
@@ -2,12 +2,11 @@ from st2reactor.sensor.base import PollingSensor
 
 
 class SampleSensor(PollingSensor):
-    def __init__(self, sensor_service, config):
-        super(SampleSensor, self).__init__(
-            sensor_service=sensor_service, config=config)
-        self._logger = self.sensor_service.get_logger(
-            name=self.__class__.__name__)
-        # self._stop = False
+    def __init__(self, sensor_service, config=None, poll_interval=5):
+        super(SampleSensor, self).__init__(sensor_service=sensor_service,
+                                           config=config,
+                                           poll_interval=poll_interval)
+        self._poll_interval = 30
 
     def setup(self):
         pass
@@ -17,7 +16,7 @@ class SampleSensor(PollingSensor):
         # count = self.sensor_service.get_value('hello_st2.count') or 0
         # payload = {'greeting': 'Yo, StackStorm!', 'count': int(count) + 1}
         payload = {"success": "ok"}
-        self.sensor_service.dispatch(
+        self._sensor_service.dispatch(
             trigger='syssignal.myevent', payload=payload)
         # self.sensor_service.set_value('hello_st2.count', payload['count'])
 
